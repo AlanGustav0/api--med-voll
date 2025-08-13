8e02d167a560c2ab7dac2d1f25bb8dbe7b9199c9
AlanGustav0 <alagustavo946@gmail.com>
2025-08-13 14:44:29 -0300

    feature: Implements new atribute telefone

--- src/main/java/med/voll/api/adapter/out/persistence/entity/MedicoEntity.java
+++ src/main/java/med/voll/api/adapter/out/persistence/entity/MedicoEntity.java
@@ -1,12 +1,13 @@
 package med.voll.api.adapter.out.persistence.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import med.voll.api.domain.model.DadosMedico;
 import med.voll.api.domain.model.Especialidade;
+import med.voll.api.domain.model.response.MedicoResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
@@ -19,6 +20,7 @@ public class MedicoEntity {
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         private String nome;
+        private String telefone;
         private String email;
         private String crm;
 
@@ -33,6 +35,7 @@ public class MedicoEntity {
                 this.nome = medico.nome();
                 this.crm = medico.crm();
                 this.email = medico.email();
+                this.telefone = medico.telefone();
                 this.especialidade = medico.especialidade();
                 this.endereco = new EnderecoEntity(medico.endereco());
         }
