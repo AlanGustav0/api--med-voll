b7b13a4448fd2fea937e84612bdfb1fbeaa4c915
AlanGustav0 <alagustavo946@gmail.com>
2025-08-13 10:15:30 -0300

    chore: Creating migrations

---
+++ src/main/resources/db/migration/V1__create-table-endereco.sql
@@ -0,0 +1,12 @@
+create table if not exists ENDERECO(
+    id bigint not null auto_increment,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null,
+
+    primary key (id)
+)
