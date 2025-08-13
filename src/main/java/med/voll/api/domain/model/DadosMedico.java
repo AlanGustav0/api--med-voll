f3b5a435fbff3e988d54432f89d2501909f5cd89
AlanGustav0 <alagustavo946@gmail.com>
2025-08-13 14:43:04 -0300

    feature: Implements Notation NotBlank e @Valid

--- src/main/java/med/voll/api/domain/model/DadosMedico.java
+++ src/main/java/med/voll/api/domain/model/DadosMedico.java
@@ -1,3 +1,22 @@
 package med.voll.api.domain.model;
 
-public record DadosMedico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco){}
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
+public record DadosMedico(
+
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        Endereco endereco){}
